63f6a8f62c0517e7001857e8c8e36d37d142fc90
lucaas-d3v <lucas.workps@gmail.com>
2026-04-10 13:12:00 -0300

    fix: alter to debain container

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,8 @@
 const std = @import("std");
-extern var environ: [*:null]?[*:0]u8;
-
 const flint = @import("flint");
 const IoHelper = flint.IoHelper;
 
 pub fn main() !void {
-    std.posix.environ = environ;
-
     var stdout_buffer: [4096]u8 = undefined;
     var stderr_buffer: [4096]u8 = undefined;
     var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
@@ -33,7 +29,9 @@ pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(gpa.allocator());
     defer arena.deinit();
 
-    flint.runCli(arena.allocator(), io) catch {
+    const alloc = arena.allocator();
+
+    flint.runCli(alloc, io) catch {
         _ = stdout_writer.interface.flush() catch {};
         _ = stderr_writer.interface.flush() catch {};
         std.process.exit(1);
